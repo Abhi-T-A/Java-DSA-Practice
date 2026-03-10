832df645335e444bd4583b8b63231f96cf2e4210
Abhi T A <abhi.t.a1806@gmail.com>
2026-03-10 18:24:58 +0530

    Add files via upload

---
+++ Introduction/AreaTriangle.java
@@ -0,0 +1,15 @@
+// import java.util.Scanner;
+
+public class AreaTriangle {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Base Value:");
+        // double base = sc.nextDouble();
+        // System.out.println("Enter Height Value:");
+        // double height = sc.nextDouble();
+        double base = 10, height = 6;
+        double area = base * height;
+        System.out.println("Area=" + area);
+        // sc.close();
+    }
+}
