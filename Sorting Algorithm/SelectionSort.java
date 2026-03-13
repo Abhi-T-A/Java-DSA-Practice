4a644f0a509eb69f392a71a6992f9e5bf83088da
Abhi T A <228365849+Abhi-T-A@users.noreply.github.com>
2026-03-13 16:27:40 +0530

    Update SelectionSort.java

--- Sorting Algorithm/SelectionSort.java
+++ Sorting Algorithm/SelectionSort.java
@@ -1,28 +1,27 @@
 
 public class SelectionSort {
+    public static void selectionsort(int[] arr) {
+        int n = arr.length;
 
-    public static void main(String[] args) {
-
-        int arr[] = { 5, 3, 8, 4, 2 };
-
-        for (int i = 0; i < arr.length - 1; i++) {
-
-            int min = i;
-
-            for (int j = i + 1; j < arr.length; j++) {
-
-                if (arr[j] < arr[min]) {
-                    min = j;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            
-            int temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
-        
-        for (int num : arr) {
-            System.out.print(num + " ");
+    }
+    public static void main(String[] args) {
+        int[] arr = { 64, 25, 12, 22, 11 };
+        selectionSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
+
